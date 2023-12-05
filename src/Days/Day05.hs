792fb1fd5336d0b2160dc6d5ff1a43831c3c65e3
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2023-12-05 09:23:36 +0000

    Day 5 Part 1

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -1,25 +1,48 @@
 module Days.Day05 where
+import           Data.Bifunctor  (bimap)
+import           Data.List       (uncons, foldl')
+import           Data.List.Split (splitOn)
+import           Data.Maybe      (fromJust)
 import qualified Program.RunDay  as R (runDay)
 import qualified Program.TestDay as T (testDay)
 import           System.Clock    (TimeSpec)
 import           Test.Hspec      (Spec)
+import           Util.Util       (listToTuple, listToTuple3)
 
 runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 testDay :: String -> String -> Spec
-testDay = T.testDay parser part1 part2 0 0
+testDay = T.testDay parser part1 part2 35 0
 
-type Input = [Int]
+data Map = Map {
+    source :: String,
+    dest   :: String,
+    ranges :: [(Int, Int, Int)]
+}
+
+type Input = ([Int], [Map])
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = bimap (map read . words . drop 6) (map parseMap) . fromJust . uncons . splitOn "\n\n"
+    where
+        parseMap :: String -> Map
+        parseMap = (\((s, d), rs) -> Map s d rs) . bimap (listToTuple . splitOn "-to-" . head . words) (map (listToTuple3 . map read . words)) . fromJust . uncons . lines
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 (nums, maps) = minimum $ map (`checkMaps` maps) nums
+    where
+        checkMaps :: Int -> [Map] -> Int
+        checkMaps = foldl' (\n -> checkMap n . ranges)
+
+checkMap :: Int -> [(Int, Int, Int)] -> Int
+checkMap n [] = n
+checkMap n ((destStart, srcStart, len):rest)
+    | n >= srcStart && n < srcStart + len = destStart - srcStart + n
+    | otherwise = checkMap n rest
 
 part2 :: Input -> Output2
 part2 = undefined
