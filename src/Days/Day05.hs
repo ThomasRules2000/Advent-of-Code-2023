6d01a2162a5d4b951496a4a4ede33b5ae38b591f
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2023-12-05 18:52:19 +0000

    [Day 5] Slow AF Part 2

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -1,7 +1,7 @@
 module Days.Day05 where
 import           Data.Bifunctor  (bimap)
 import           Data.List       (uncons, foldl')
-import           Data.List.Split (splitOn)
+import           Data.List.Split (splitOn, chunksOf)
 import           Data.Maybe      (fromJust)
 import qualified Program.RunDay  as R (runDay)
 import qualified Program.TestDay as T (testDay)
@@ -15,13 +15,7 @@ runDay = R.runDay parser part1 part2
 testDay :: String -> String -> Spec
 testDay = T.testDay parser part1 part2 35 0
 
-data Map = Map {
-    source :: String,
-    dest   :: String,
-    ranges :: [(Int, Int, Int)]
-}
-
-type Input = ([Int], [Map])
+type Input = ([Int], [[(Int, Int, Int)]])
 
 type Output1 = Int
 type Output2 = Int
@@ -29,20 +23,22 @@ type Output2 = Int
 parser :: String -> Input
 parser = bimap (map read . words . drop 6) (map parseMap) . fromJust . uncons . splitOn "\n\n"
     where
-        parseMap :: String -> Map
-        parseMap = (\((s, d), rs) -> Map s d rs) . bimap (listToTuple . splitOn "-to-" . head . words) (map (listToTuple3 . map read . words)) . fromJust . uncons . lines
+        parseMap :: String -> [(Int, Int, Int)]
+        parseMap = map (listToTuple3 . map read . words) . tail . lines
 
 part1 :: Input -> Output1
 part1 (nums, maps) = minimum $ map (`checkMaps` maps) nums
-    where
-        checkMaps :: Int -> [Map] -> Int
-        checkMaps = foldl' (\n -> checkMap n . ranges)
 
-checkMap :: Int -> [(Int, Int, Int)] -> Int
-checkMap n [] = n
-checkMap n ((destStart, srcStart, len):rest)
-    | n >= srcStart && n < srcStart + len = destStart - srcStart + n
-    | otherwise = checkMap n rest
+checkMaps :: Int -> [[(Int, Int, Int)]] -> Int
+checkMaps = foldl' go
+    where
+        go :: Int -> [(Int, Int, Int)] -> Int
+        go n [] = n
+        go n ((destStart, srcStart, len):rest)
+            | n >= srcStart && n < srcStart + len = destStart - srcStart + n
+            | otherwise = go n rest
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 (xs, maps) = minimum $ map (`checkMaps` maps) nums
+    where
+        nums = concatMap (\[x,y] -> [x..x+y]) $ chunksOf 2 xs
