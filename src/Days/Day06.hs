33ccc22c2da559d3e737b288dc64eaa5ba3d0f88
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2023-12-06 10:18:12 +0000

    Day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,8 +1,10 @@
 module Days.Day06 where
+import           Data.List       (transpose)
 import qualified Program.RunDay  as R (runDay)
 import qualified Program.TestDay as T (testDay)
 import           System.Clock    (TimeSpec)
 import           Test.Hspec      (Spec)
+import           Util.Util       (listToTuple)
 
 runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
@@ -10,16 +12,32 @@ runDay = R.runDay parser part1 part2
 testDay :: String -> String -> Spec
 testDay = T.testDay parser part1 part2 0 0
 
-type Input = [Int]
+type Input = [[String]]
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser =  map (tail . words) . lines
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 = product . map (uncurry numWins . listToTuple . map read) . transpose
+
+numWins :: Int -> Int -> Int
+numWins time minDist = right' - left' + 1
+    where
+        -- tx - x^2 > minDist
+        -- x^2 - tx + minDist < 0
+        discriminant :: Double
+        discriminant = sqrt $ fromIntegral $ (time * time) - (4 * minDist)
+
+        -- Compute roots and round them inwards
+        left  = ceiling $ (fromIntegral time - discriminant) / 2
+        right = floor   $ (fromIntegral time + discriminant) / 2
+
+        -- Account for equality
+        left'  = if left  * (time-left)  == minDist then left  + 1 else left
+        right' = if right * (time-right) == minDist then right - 1 else right
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 = uncurry numWins . listToTuple . map (read . concat)
