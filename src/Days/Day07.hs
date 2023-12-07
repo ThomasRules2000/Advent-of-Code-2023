c75e525521d6adc6794ac9d30d941a3e70638892
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2023-12-07 23:49:14 +0000

    Day 7 Part 2

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -1,6 +1,8 @@
 module Days.Day07 where
+import           Control.Arrow   ((>>>))
 import           Control.DeepSeq (NFData)
 import           Data.Bifunctor  (bimap)
+import           Data.Function   (on)
 import           Data.List       (group, sort, sortBy)
 import           GHC.Generics    (Generic)
 import qualified Program.RunDay  as R (runDay)
@@ -13,10 +15,13 @@ runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 testDay :: String -> String -> Spec
-testDay = T.testDay parser part1 part2 0 0
+testDay = T.testDay parser part1 part2 6440 5905
 
 data Card = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
     deriving (Eq, Ord, Show, Generic, NFData)
+
+data Score = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind
+    deriving (Eq, Ord, Show)
 
 type Input = [([Card], Int)]
 
@@ -43,28 +48,32 @@ getCard 'A' = Ace
 getCard _   = undefined
 
 part1 :: Input -> Output1
-part1 = sum . zipWith (*) [1..] . map snd . sortBy compareHands
+part1 = solve id compare
 
-scoreHand :: [Card] -> Int
-scoreHand hand
-    | groupSizes == [5]       = 7
-    | groupSizes == [1, 4]    = 6
-    | groupSizes == [2, 3]    = 5
-    | groupSizes == [1, 1, 3] = 4
-    | groupSizes == [1, 2, 2] = 3
-    | groupSizes == [1,1,1,2] = 2
-    | otherwise               = 1
-    where groupSizes = sort $ map length $ group $ sort hand
+solve :: ([Card] -> [Card]) -> (Card -> Card -> Ordering) -> [([Card], Int)] -> Int
+solve filterFunc comparator = sum . zipWith (*) [1..] . map snd . sortBy (compareHands filterFunc comparator `on` fst)
 
-compareHands :: ([Card], Int) -> ([Card], Int) -> Ordering
-compareHands (h1, _) (h2, _)
-    | h1 == h2 = EQ
-    | h1Score > h2Score = GT
-    | h1Score < h2Score = LT
-    | otherwise = head $ filter (/=EQ) $ zipWith compare h1 h2
-    where
-        h1Score = scoreHand h1
-        h2Score = scoreHand h2
+compareHands :: ([Card] -> [Card]) -> (Card -> Card -> Ordering) -> [Card] -> [Card] -> Ordering
+compareHands filterFunc comparator h1 h2 = case (compare `on` (scoreHand . filterFunc)) h1 h2 of
+        EQ -> head $ dropWhile (==EQ) $ zipWith comparator h1 h2
+        x  -> x
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 = solve (filter (/=Jack)) cmpJokerWorst
+
+cmpJokerWorst :: Card -> Card -> Ordering
+cmpJokerWorst Jack Jack = EQ
+cmpJokerWorst Jack _    = LT
+cmpJokerWorst _ Jack    = GT
+cmpJokerWorst x y       = compare x y
+
+scoreHand :: [Card] -> Score
+scoreHand = sort >>> group >>> map length >>> sort >>> \case
+    []           -> FiveOfAKind  -- Hand of Jokers
+    [_]          -> FiveOfAKind  -- Make all Jokers behave as single other card
+    [1, _]       -> FourOfAKind  -- If the lowest number of another card type is 1, we can make 4 of a kind
+    [_, _]       -> FullHouse    -- Otherwise this must be 2, so we make a full house
+    [1, 1, _]    -> ThreeOfAKind -- If we have 3 types of card and 1 each of the two smallest, we can make 3 of a kind
+    [_, _, _]    -> TwoPair      -- Otherwise we must already have a 2 pair
+    [_, _, _, _] -> OnePair      -- Exactly 4 different types of card can only produce a single pair
+    _            -> HighCard     -- This must be exactly 5 types of card
