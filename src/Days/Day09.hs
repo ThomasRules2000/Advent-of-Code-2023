89a7c0ce4da717da96351e5f3a771d0a4234fd20
Thomas Stacey <Thomas@ThomasStacey.co.uk>
2023-12-09 15:15:01 +0000

    Day 9

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -8,18 +8,27 @@ runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 testDay :: String -> String -> Spec
-testDay = T.testDay parser part1 part2 0 0
+testDay = T.testDay parser part1 part2 114 2
 
-type Input = [Int]
+type Input = [[Int]]
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = map (map read . words) . lines
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 = solve . map reverse
+
+solve :: [[Int]] -> Int
+solve = sum . map extrapolate
+    where
+        extrapolate :: [Int] -> Int
+        extrapolate xs
+            | all (==0) diffs = head xs
+            | otherwise = head xs - extrapolate diffs
+            where diffs = zipWith subtract xs (tail xs)
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 = solve
